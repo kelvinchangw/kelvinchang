4b8f25838ecee6c52d7298c976afe2fcad032904
Weissey <kelvinchangw@gmail.com>
2024-02-19 17:08:51 -0800

    Implement smooth scrolling using Lenis & GSAP

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
+// IMPORTS
 import "./style.css";
+
+// Lenis Smooth Scroll
+import Lenis from '@studio-freight/lenis';
+
+// GSAP ScrollTrigger
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
+
+// 
+const lenis = new Lenis();
+
+lenis.on('scroll', (e) => {
+  console.log(e);
+})
+
+lenis.on('scroll', ScrollTrigger.update);
+
+gsap.ticker.add((time)=>{
+  lenis.raf(time * 1000);
+})
+
+gsap.ticker.lagSmoothing(0);
