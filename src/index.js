0e407773e0f093090cc0cd425cb8b22750dafc08
Weissey <kelvinchangw@gmail.com>
2024-02-19 23:58:38 -0800

    Begin implementation of smooth anchor links using Lenis

--- src/index.js
+++ src/index.js
@@ -14,13 +14,18 @@ import { ScrollToPlugin } from "gsap/ScrollToPlugin";
 gsap.registerPlugin(ScrollTrigger, TextPlugin, ScrollToPlugin);
 
 // Lenis + GSAP ScrollTrigger
-const lenis = new Lenis();
+const lenis = new Lenis({
+    duration: 1.2,
+    wheelMultiplier: 2,
+});
 
 lenis.on("scroll", (e) => {
     console.log(e);
 });
 
 lenis.on("scroll", ScrollTrigger.update);
+
+// lenis.on("scroll", ScrollTrigger.refresh);
 
 gsap.ticker.add((time) => {
     lenis.raf(time * 1000);
@@ -30,16 +35,54 @@ gsap.ticker.lagSmoothing(0);
 
 // Replaces introTitle (which is an empty space) with "Hello, I am Kelvin."
 const introTitle = document.querySelector(".intro-title");
+
+const introLink = document.querySelector(".nav-link.intro");
+
+// introLink.addEventListener("click", () => {
+//     lenis.scrollTo("#intro");
+// });
+
+// DYNAMIC UPDATES OF STUFF BELOW
+// https://gsap.com/community/forums/topic/37417-disabling-scrolltrigger-onleave-and-re-enabling-onleaveback/
+
+// ScrollTrigger.create({
+//     trigger: introTitle,
+//     start: "top 46%", // Trigger point
+//     end: "top top", // Define an end point to ensure onLeaveBack is called
+//     markers: true, // For debugging, remove in production
+//     toggleActions: "play none none none", // Adjust if needed
+//     onEnter: () => updateText("Hello, I am Kelvin."), // Text for scrolling down through 50%
+//     onLeaveBack: () => updateText("KELVIN KELVIN KELVIN"), // Text for scrolling back up through 50%
+//     // Optional: Adjust based on your needs
+//     onEnterBack: () => updateText("Original Text"),
+//     onLeave: () => updateText("Another Text"),
+// });
+
+// // Function to update text
+// function updateText(newText) {
+//     gsap.to(introTitle, {
+//         duration: 2,
+//         text: { value: newText },
+//         ease: "none", // Customize the easing if needed
+//     });
+// }
+
+// // Scroll to top on page load
+// window.addEventListener("load", () => {
+//     lenis.scrollTo(0);
+// });
+
 gsap.to(introTitle, {
-    scrollTrigger: introTitle,
-    duration: 1.6,
+    duration: 2,
     text: {
         value: "Hello, I am Kelvin.",
     },
     ease: "none",
 });
 
-// Scroll to top on page load
-window.addEventListener("load", () => {
-    gsap.to(window, { duration: 0.6, scrollTo: 0, ease: "expo.inOut" });
+document.querySelectorAll('.nav-link').forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+        lenis.scrollTo(this.getAttribute("href"));
+    });
 });
