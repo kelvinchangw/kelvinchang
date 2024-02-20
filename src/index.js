9c81a910e2c465b72412fb19d1e1e6a8b3069c08
Weissey <kelvinchangw@gmail.com>
2024-02-19 19:19:14 -0800

    Implement intro text animation with TextPlugin from GSAP

--- src/index.js
+++ src/index.js
@@ -2,25 +2,44 @@
 import "./style.css";
 
 // Lenis Smooth Scroll
-import Lenis from '@studio-freight/lenis';
+import Lenis from "@studio-freight/lenis";
 
 // GSAP ScrollTrigger
 import { gsap } from "gsap";
+
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-gsap.registerPlugin(ScrollTrigger);
+import { TextPlugin } from "gsap/TextPlugin";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
 
+gsap.registerPlugin(ScrollTrigger, TextPlugin, ScrollToPlugin);
 
-// 
+// Lenis + GSAP ScrollTrigger
 const lenis = new Lenis();
 
-lenis.on('scroll', (e) => {
-  console.log(e);
-})
+lenis.on("scroll", (e) => {
+    console.log(e);
+});
 
-lenis.on('scroll', ScrollTrigger.update);
+lenis.on("scroll", ScrollTrigger.update);
 
-gsap.ticker.add((time)=>{
-  lenis.raf(time * 1000);
-})
+gsap.ticker.add((time) => {
+    lenis.raf(time * 1000);
+});
 
 gsap.ticker.lagSmoothing(0);
+
+// Replaces introTitle (which is an empty space) with "Hello, I am Kelvin."
+const introTitle = document.querySelector(".intro-title");
+gsap.to(introTitle, {
+    scrollTrigger: introTitle,
+    duration: 1.6,
+    text: {
+        value: "Hello, I am Kelvin.",
+    },
+    ease: "none",
+});
+
+// Scroll to top on page load
+window.addEventListener("load", () => {
+    gsap.to(window, { duration: 0.6, scrollTo: 0, ease: "expo.inOut" });
+});
