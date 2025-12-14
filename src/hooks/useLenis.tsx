db2d01b94973458c0118c9af0feeb8fdd92ed150
Kelvin Chang <kelvinchangw@gmail.com>
2025-12-14 02:04:19 -0800

    Increase responsiveness of scrolling

--- src/hooks/useLenis.tsx
+++ src/hooks/useLenis.tsx
@@ -4,7 +4,7 @@ import { useEffect } from 'react'
 export default function useLenis() {
     useEffect(() => {
         const lenis = new Lenis({
-            duration: 1,
+            duration: 0.8,
             // easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
             orientation: 'vertical',
             gestureOrientation: 'vertical',
