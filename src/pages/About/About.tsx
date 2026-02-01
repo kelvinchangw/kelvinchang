2c1cc6bb8618fde81309c8ba301ab38c8f83f1da
Kelvin <kelvinchangw@gmail.com>
2026-02-01 02:23:03 -0800

    Add music section to About page

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -2,6 +2,12 @@ import styles from "./About.module.css";
 import divider from "../../assets/images/divider.png";
 import pfp from "../../assets/images/pfp.png";
 
+import djTurnItUpCoverArt from "../../assets/images/djturnitup-cover.jpg";
+import solarSystemCoverArt from "../../assets/images/solarsystem-cover.jpg";
+import cityRuinsCoverArt from "../../assets/images/cityruins-cover.jpg";
+import blueCoverArt from "../../assets/images/blue-cover.jpg";
+import howSweetCoverArt from "../../assets/images/howsweet-cover.jpg";
+
 export default function About() {
     return (
         <main className={styles.page}>
@@ -53,6 +59,89 @@ export default function About() {
                         <span>
                             I love hotpot, lamb skewers, and DnB. Mmmmmmmmm.
                         </span>
+                    </div>
+                </div>
+                <div className={styles.music}>
+                    <span className={styles.title}>music</span>
+                    <img src={divider} className={styles.divider} />
+                    <div className={styles.musicinfo}>
+                        <span>
+                            This is a section dedicated to my favorite music.
+                            Every song here has impacted me significantly in
+                            some way.
+                        </span>
+                    </div>
+                    <div className={styles.songContainer}>
+                        <div className={styles.song}>
+                            <div className={styles.coverArtContainer}>
+                                <img
+                                    src={djTurnItUpCoverArt}
+                                    className={styles.coverArt}
+                                />
+                            </div>
+                            <span className={styles.songTitle}>
+                                Dimension - DJ Turn It Up
+                            </span>
+                            <span className={styles.songComment}>
+                                "Redefined what dance music meant to me"
+                            </span>
+                        </div>
+                        <div className={styles.song}>
+                            <div className={styles.coverArtContainer}>
+                                <img
+                                    src={solarSystemCoverArt}
+                                    className={styles.coverArt}
+                                />
+                            </div>
+                            <span className={styles.songTitle}>
+                                Sub Focus - Solar System
+                            </span>
+                            <span className={styles.songComment}>
+                                "Started my obsession with the DNB genre"
+                            </span>
+                        </div>
+                        <div className={styles.song}>
+                            <div className={styles.coverArtContainer}>
+                                <img
+                                    src={cityRuinsCoverArt}
+                                    className={styles.coverArt}
+                                />
+                            </div>
+                            <span className={styles.songTitle}>
+                                Nier OST - City Ruins (Shade)
+                            </span>
+                            <span className={styles.songComment}>
+                                "Influenced my love for ambience and atmosphere"
+                            </span>
+                        </div>
+                        <div className={styles.song}>
+                            <div className={styles.coverArtContainer}>
+                                <img
+                                    src={blueCoverArt}
+                                    className={styles.coverArt}
+                                />
+                            </div>
+                            <span className={styles.songTitle}>
+                                Yoasobi - Blue
+                            </span>
+                            <span className={styles.songComment}>
+                                "Means something very special to me"
+                            </span>
+                        </div>
+                        <div className={styles.song}>
+                            <div className={styles.coverArtContainer}>
+                                <img
+                                    src={howSweetCoverArt}
+                                    className={styles.coverArt}
+                                />
+                            </div>
+                            <span className={styles.songTitle}>
+                                NewJeans - How Sweet
+                            </span>
+                            <span className={styles.songComment}>
+                                "Marks a period of heavy nostalgia"
+                            </span>
+                        </div>
                     </div>
                 </div>
             </div>
